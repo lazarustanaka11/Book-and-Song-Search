2c32f226ca5120306c5c423a4fda534a992d5dbe
lazarustanaka11 <lazarusmtake@gmail.com>
2021-08-21 09:51:13 +0200

    changed directory structure

--- app/src/itunes.py
+++ app/src/itunes.py
@@ -1,41 +1,48 @@
 import init_request
 
+
 class Itunes(object):
     def __init__(self):
         super().__init__()
         self.initiate_calls = init_request.initiate_calls()
-    def song_search(self,term):
+
+    def song_search(self, term):
         self.term = term
         self.song_base_url = "https://itunes.apple.com/search?"
-        self.song_payload = {"term": self.term, "limit":5}
-        items = []#empty list to which the results will be appended
-        #time the function call below
-        results, song_result_status,song_time_lapse = self.initiate_calls.init_request(self.song_base_url
-                                                                                      ,self.song_payload)
-        #filter the desired result from the itunes API call result
+        self.song_payload = {"term": self.term, "limit": 5}
+        items = []  # empty list to which the results will be appended
+        # time the function call below
+        results, song_result_status, song_time_lapse = self.initiate_calls.init_request(
+            self.song_base_url, self.song_payload
+        )
+        # filter the desired result from the itunes API call result
         if song_result_status == 200:
             if "resultCount" in results and results["resultCount"] > 0:
                 results = results["results"]
             else:
                 results = []
-        else :
-            #incase of failure, just return an empty list
+        else:
+            # incase of failure, just return an empty list
             results = []
         """
             - If there is something in results list, look for kind and artist and add to list.
             - If there is nothing, use the defualt,empty items list initialized above
         """
         if len(results) > 0:
-            
+
             for each_item in results:
-                each_item_kind = each_item["kind"] if "kind" in each_item else each_item["wrapperType"]
+                each_item_kind = (
+                    each_item["kind"]
+                    if "kind" in each_item
+                    else each_item["wrapperType"]
+                )
                 artist = each_item["artistName"]
                 if "trackName" in each_item:
                     title = each_item["trackName"]
                 else:
-                    #IF THERE IS NO TITLE, APPEND 3 *S AND EXPLAIN IN DOCUMENTATION
+                    # IF THERE IS NO TITLE, APPEND 3 *S AND EXPLAIN IN DOCUMENTATION
                     title = "***"
                     title += each_item["collectionName"]
-                items.append({"type":each_item_kind,"artist":artist,"title":title})
-        #return the results list, status code of call and the time lapse
-        return items, song_result_status , song_time_lapse
+                items.append({"type": each_item_kind, "artist": artist, "title": title})
+        # return the results list, status code of call and the time lapse
+        return items, song_result_status, song_time_lapse
